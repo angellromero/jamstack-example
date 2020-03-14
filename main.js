44dfb0f4b77ce33c394e338fb276c74bc0245530
Angel Romero <solo1artist@gmail.com>
2020-03-14 15:08:43 -0500

    Initial Commit

---
+++ main.js
@@ -0,0 +1,23 @@
+const listRepos = async username => {
+  const repos = await fetch(
+    `https://api.github.com/users/${username}/repos?type=owner&sort=updated`
+  )
+    .then(res => res.json())
+    .catch(error => console.error(error));
+
+  const content = document.getElementById("content");
+
+  const markup = repos
+    .map(
+      repo => `
+    <li>
+      <a href="${repo.html_url}">${repo.name}</a> 
+      (⭐️ ${repo.stargazers_count})
+    </li>
+  `
+    )
+    .join("");
+  content.innerHTML = `<ul>${markup}</ul>`;
+};
+
+listRepos("jlengstorf");
